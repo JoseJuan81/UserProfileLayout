382b7ec10ced003a607807923cc50adfdb627b52
josejuan81 <pe.dominguezjosejuan@gmail.com>
2020-01-19 17:31:58 -0500

    rollup implementacion

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import UserProfileLayout from './components/UserProfileLayout.vue'
+
+export function install(Vue) {
+	if (install.intalled) return;
+	install.installed = true;
+	Vue.component('UserProfileLayout', UserProfileLayout);
+}
+
+const plugin = {
+	install,
+};
+
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+	GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+	GlobalVue = global.Vue;
+}
+
+if (GlobalVue) {
+	GlobalVue.use(plugin);
+}
+
+export default UserProfileLayout;
